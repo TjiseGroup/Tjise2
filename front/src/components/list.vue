780592620066c2729e9e977c80ce2d1a57414bad
youngckjer <35186251+youngckjer@users.noreply.github.com>
2018-04-28 09:49:24 +0800

    test
    test

--- front/src/components/list.vue
+++ front/src/components/list.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
+  <div class="list">
       list
+    
   </div>
 </template>
 
@@ -16,5 +17,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.list{
+	color: #fff;
+}
 </style>
